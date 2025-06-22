54820204fdac60fa23f77d1f50bbd7c94e2e392d
Anidipta <anidiptapal@gmail.com>
2025-06-23 00:18:15 +0530

    del

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .segmentation import SegmentationModel
-from .flood_detection import FloodDetectionModel
-from .person_detection import PersonDetectionModel
-
-__all__ = ['SegmentationModel', 'FloodDetectionModel', 'PersonDetectionModel']
